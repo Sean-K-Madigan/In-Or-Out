2d69402df3b178646e2ff5baaf6d8f9c2b6d330a
Tish <tishthegoat@gmail.com>
2024-06-08 09:19:13 -0600

    models & associations

---
+++ models/index.js
@@ -0,0 +1,34 @@
+const User = require('./User')
+const Event = require('./Event')
+
+User.hasMany(Event, {
+	foreignKey: 'creator_id',
+	onDelete: 'CASCADE'
+})
+
+Event.belongsTo(User, {
+	foreignKey: 'creator_id'
+})
+
+User.belongsToMany(User, {
+	as: 'Friends',
+	through: 'network', 
+	foreignKey: 'user_id',
+	otherKey: 'friend_id'
+})
+
+User.belongsToMany(Event, {
+	as: 'Participents',
+	through: 'UserEvent', 
+	foreignKey: 'user_id',
+	otherKey: 'event_id'
+})
+
+
+Event.belongsToMany(User, {
+	through: 'UserEvent',
+	foreignKey: 'event_id',
+	otherKey: 'user_id'
+})
+
+module.exports = { User, Event }
