c2b3527e24309edfd66084cf779a819e9323b308
Tish <tishthegoat@gmail.com>
2024-06-12 08:36:49 -0600

    search by user

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const Event = require('../models/Event')
 const User = require('../models/User')
 const Sequelize = require('sequelize')
+const color = require('colors')
 
 
 // render events
@@ -26,11 +27,11 @@ router.get('/', async (req, res) => {
 		
 		
 		//for checking if logged in
-		const context = {
-			events: events,
-			logged_in: req.session.logged_in
-		}
-		console.log(context)
+		// const context = {
+		// 	events: events,
+		// 	logged_in: req.session.logged_in
+		// }
+		// console.log(context)
 		
 		res.render('homepage', { events,
 			logged_in: req.session.logged_in
@@ -41,24 +42,37 @@ router.get('/', async (req, res) => {
 	}
 })
 
-// router.get('/search') = {
-// 	searchUser()
-// }
-// const searchUser = async (req, res) => {
-// 	const { term } = req.query
-// if(!term){
-// 	// todo make error handler to prompt-for res.send
-// 	res.status(400).json({ message: 'No search term provided' })
-// 	return
-// }
-// const searchData = User.findAll({
-// 	where: {
-// 		[Sequelize.Op.or]: [
-// 			{ username: { [Sequelize.Op.like]: `%${term}%` } },
-// 			{ email: { [Sequelize.Op.like]: `%${term}%` } },
-// 			{ first_name: { [Sequelize.Op.like]: `%${term}%` } },
-// 			{ hobbies: { [Sequelize.Op.like]: `%${term}%` } }
-// 		]
-// 	}
-// })
+router.get('/search', searchUser)
+
+async function searchUser(req, res){
+	try {
+		console.log('req.query'.green, req.query)
+		const { search } = req.query
+		console.log(search)
+
+		const searchData = await User.findAll({
+			where: {
+				[Sequelize.Op.or]: [
+					{ username: { [Sequelize.Op.like]: `%${search}%` } },
+					{ email: { [Sequelize.Op.like]: `%${search}%` } },
+					{ name: { [Sequelize.Op.like]: `%${search}%` } },
+					{ hobbies: { [Sequelize.Op.like]: `%${search}%` } }
+				]
+			},
+			attributes: { exclude: ['password'] }
+		})
+
+		if(!searchData){
+			res.status(404).json({ message: 'No users found' })
+			return
+		}
+			const userResults = searchData.map(user => user.get({ plain: true }))
+
+			res.status(200).json({ users: userResults})
+
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({message:`Error occured when trying to search`, error})
+	}
+}
 module.exports = router
