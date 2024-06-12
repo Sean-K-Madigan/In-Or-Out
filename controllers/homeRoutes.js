22df07ead7d73f7647f467ca49bda484de8b4ff8
Tish <tishthegoat@gmail.com>
2024-06-12 09:35:40 -0600

    search by user || event

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,28 +27,41 @@ router.get('/', async (req, res) => {
 		
 		
 		//for checking if logged in
-		// const context = {
-		// 	events: events,
-		// 	// logged_in: req.session.logged_in
-		// }
-		// console.log(context)
+		const context = {
+			events: events,
+			// logged_in: req.session.logged_in
+		}
+		console.log(context)
 		
-		res.render('homepage', { events,
-			logged_in: req.session.logged_in
-		})
+		// res.render('homepage', { events,
+		// 	logged_in: req.session.logged_in
+		// })
 		// res.status(200).json(events)
 	} catch (error) {
 		console.log(`Error occured when trying to get all events`.red, error)
 	}
 })
 
-router.get('/search', searchUser, searchEvent)
+router.get('/search', async (req, res) => {
+	try {
+		const users = await searchUser(req, res)
+		const events = await searchEvent(req, res)
+		console.log('Users'.green, users, `Events`.green, events)
+
+		if(users || events){
+			res.status(200).json({ users:users, events:events })
+			return
+			}
+			res.status(404).json({ message: 'No results found' })
+		
+	} catch (error) {
+		res.status(500).json({ message: 'Error occured when trying to search', error })
+	}
+})
 // search for users
 async function searchUser(req, res){
-	try {
-		console.log('req.query'.green, req.query)
+		console.log('User req.query'.green, req.query)
 		const { search } = req.query
-		console.log(search)
 
 		const searchData = await User.findAll({
 			where: {
@@ -62,48 +75,34 @@ async function searchUser(req, res){
 			attributes: { exclude: ['password'] }
 		})
 
-		if(!searchData){
-			res.status(404).json({ message: 'No users found' })
-			return
+		if(searchData.length < 1){
+			return null
 		}
 			const userResults = searchData.map(user => user.get({ plain: true }))
+			return userResults
 
-			res.status(200).json({ users: userResults})
-
-	} catch (error) {
-		console.log(error)
-		res.status(500).json({message:`Error occured when trying to search`, error})
-	}
 }
 async function searchEvent(req, res){
-	try {
-		console.log('req.query'.green, req.query)
+		console.log('Event req.query'.green, req.query)
 		const { search } = req.query
-		console.log(search)
 
 		const searchData = await Event.findAll({
 			where: {
 				[Sequelize.Op.or]: [
 					{ title: { [Sequelize.Op.like]: `%${search}%` } },
 					{ category: { [Sequelize.Op.like]: `%${search}%` } },
-					{ date: { [Sequelize.Op.like]: `%${search}%` } },
+					// todo look into how to search this.
+					// { date: { [Sequelize.Op.like]: `%${search}%` } },
 					{ created_by: { [Sequelize.Op.like]: `%${search}%` } }
 				]
 			},
-			attributes: { exclude: ['password'] }
 		})
 
-		if(!searchData){
-			res.status(404).json({ message: 'No users found' })
-			return
+		if(searchData.length < 1){
+			return null
 		}
-			const userResults = searchData.map(user => user.get({ plain: true }))
-
-			res.status(200).json({ users: userResults})
-
-	} catch (error) {
-		console.log(error)
-		res.status(500).json({message:`Error occured when trying to search`, error})
-	}
+			const eventResults = searchData.map(event => event.get({ plain: true }))
+			return eventResults
+	
 }
 module.exports = router
