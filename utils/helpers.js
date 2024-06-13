a51ed2b3ccceea4fdf21e8d06316c3782f9c9494
Sean-K-Madigan <157637384+Sean-K-Madigan@users.noreply.github.com>
2024-06-13 16:16:24 -0600

    add formatDate helper to helper js and import it into server for app useage

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,3 +8,40 @@ next();
 }
 
 module.exports = reqLog
+
+
+
+function formatDate(date) {
+
+  // Get month, day, year, hours, and minutes
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
+  var year = date.getFullYear() % 100; // Get last two digits of the year
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+
+  // Determine AM/PM
+  var period = hours >= 12 ? "PM" : "AM";
+
+  // Convert hours to 12-hour format
+  hours = hours > 12 ? hours - 12 : hours;
+  hours = hours == 0 ? 12 : hours;
+
+  // Add leading zeros if needed
+  month = month < 10 ? "0" + month : month;
+  day = day < 10 ? "0" + day : day;
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  // Construct formatted date string
+  var formattedDate =
+    month + "/" + day + "/" + year + " " + hours + ":" + minutes + " " + period;
+  return formattedDate;
+}
+
+// Example usage:
+// var currentDate = new Date();
+// console.log(formatDate(currentDate));
+
+module.exports = formatDate;
+
